c98976620c420989ae45b3dfdf093c532ba5ba0c
neg._.ah <127397572+neg2004ah@users.noreply.github.com>
2023-03-12 16:22:22 +0330

    create template root
    template folder

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR.joinpath('template'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'test.html')
+    return render(request, 'home/index.html')
